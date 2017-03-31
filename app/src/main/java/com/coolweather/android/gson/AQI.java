1dfcff6f6de1970342d9d7201f4bcd642c1d2381
DesiNF <a312567415@qq.com>
2017-03-31 11:24:13 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/3/30 0030.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
